44eb9f82f96cde7cfac1d316b2350aea25c7997a
Jimin Kang <jimin.kang91@gmail.com>
2017-12-20 21:00:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var empArry [];
   for(var i=1; i<musicians.length();i++){
-    empArry.push(`${musicians[i]} plays ${instruments[i]}`)
+    empArry.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return empArry;
 }
